edeeced829e838022256ed4a915f027b3f1e31b6
cagelight <kallikrates.koios@gmail.com>
2017-02-12 23:55:07 -0600

    fixed HTML validation messing with JSON output

--- ParaJSON.php
+++ ParaJSON.php
@@ -1,5 +1,6 @@
 ﻿<?php
 
+header("Content-Type: application/json");
 ob_start();
 
 $safeToExecuteParaFunc = "1";
@@ -7,7 +8,6 @@ include 'ParaFunc.php';
 
 checkForAndDoUpdateIfNecessary($serverIPAddress, $serverPort, $dynamicIPAddressPath, $floodProtectTimeout, $connectionTimeout, $refreshTimeout, $disableFrameBorder, $fadeLevelshots, $levelshotDisplayTime, $levelshotTransitionTime, $levelshotFPS, $maximumLevelshots, $levelshotFolder, $filterOffendingServerNameSymbols, $gameName, $noPlayersOnlineMessage, $enableAutoRefresh, $autoRefreshTimer, $maximumServerInfoSize, $RConEnable, $RConMaximumMessageSize, $RConFloodProtect, $RConLogSize, $newWindowSnapToCorner, $dynamicTrackerEnabled);
 $dump = file_get_contents("info/" . $dynamicIPAddressPath . "serverDump.txt");
-$dump = stringValidator( $dump, "", "");
 $dump_split = explode("\n", $dump);
 array_pop($dump_split);
 $server_info = explode('\\', $dump_split[1]);
@@ -15,22 +15,21 @@ array_shift($server_info);
 $json = "{\"version\":\"". versionNumber() . "\",\"info\":{";
 for ($i = 0; $i < count($server_info); $i += 2) {
   if ($i != 0) { $json .= ",";}
-  $json .= '"' . $server_info[$i] . '":"' . $server_info[$i+1] . '"';
+  $json .= '"' . stringValidator($server_info[$i], "", "") . '":"' . stringValidator($server_info[$i+1], "", "") . '"';
 }
 $json .= "},\"players\":[";
 for ($i = 2; $i < count($dump_split); $i ++) {
   $player_split = explode(' ', $dump_split[$i], 3);
   if ($i != 2) { $json .= ",";}
-  $json .= "{\"name\":" . $player_split[2] . ",";
-  $json .= "\"score\":" . $player_split[0] . ",";
-  $json .= "\"ping\":" . $player_split[1] . "}";
+  $json .= "{\"name\":\"" . stringValidator(trim($player_split[2],"\""), "", "") . "\",";
+  $json .= "\"score\":" . stringValidator($player_split[0], "", "") . ",";
+  $json .= "\"ping\":" . stringValidator($player_split[1], "", "") . "}";
 }
 $json .= "]}";
 
 ob_clean();
 ob_end_flush();
 
-header("Content-Type: application/json");
 echo $json
 
 ?>
